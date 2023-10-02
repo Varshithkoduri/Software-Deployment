70d15f19e5f64d85da682a407e2e9432c54f887c
Varshithkoduri <146044412+Varshithkoduri@users.noreply.github.com>
2023-10-01 20:05:29 -0500

    Updated LetterGrade.java
    I have modified the initial LetterGrade.java file to include the below options:
    A+
    B+
    C+
    D+

--- LetterGrade.java
+++ LetterGrade.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class LetterGrade{
     public static void main(String[] args){
-        char letterGrade;
+        String letterGrade;
 
         System.out.println("Hi.., This is Team KANYARASI \n Team Leaders : Varshith, Ashok");
         System.out.println("Please enter your grade : ");
@@ -14,29 +14,50 @@ public class LetterGrade{
 
         int grade = scan.nextInt();
 
+        //if grade >= 95 , set the letterGrade to A+
+        if(grade >= 95){
+            letterGrade = "A+";
+        }
+
         //if grade >= 90 , set the letterGrade to A
-        if(grade >= 90){
-            letterGrade = 'A';
+        else if(grade >= 90){
+            letterGrade = "A";
+        }
+
+        //if grade >= 85 , set the letterGrade to B+
+        else if(grade >= 85){
+            letterGrade = "B+";
         }
 
         //if grade >= 80 , set the letterGrade to B
         else if(grade >= 80){
-            letterGrade = 'B';
+            letterGrade = "B";
         }
+
+        //if grade >= 75 , set the letterGrade to C+
+        else if(grade >= 75){
+            letterGrade = "C+";
+        }
+
 
         //if grade >= 70 , set the letterGrade to C
         else if(grade >= 70){
-            letterGrade = 'C';
+            letterGrade = "C";
+        }
+
+        //if grade >= 65 , set the letterGrade to D+
+        else if(grade >= 65){
+            letterGrade = "D+";
         }
 
         //if grade >= 60 , set the letterGrade to D
         else if(grade >= 60){
-            letterGrade = 'D';
+            letterGrade = "D";
         }
 
         //else set the letterGrade to F
         else{
-            letterGrade = 'F';
+            letterGrade = "F";
         }
         
         System.out.println("Your Final grade is : " + letterGrade);
